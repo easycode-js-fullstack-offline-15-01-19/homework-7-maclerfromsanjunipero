8366e16f12fe433c04e595d7d1dc362e71035945
maclerfromsanjunipero <vasilenko.maxim5@gmail.com>
2019-02-15 01:15:11 +0200

    Vasilenko Max HW#7

--- homework.js
+++ homework.js
@@ -1,2 +1,39 @@
-// -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Home work#7 --------------------------
+// -------------------------- Vasilenko Max --------------------------
+
+// Destruction properties.Tasks
+// Task#1
+
+function newObj() {
+    let [a, ...rest] = arguments;
+    let newObj = {
+        first: a,
+        othets: rest
+    };
+    return newObj
+}
+
+//Task#2
+
+const organisation = {
+    name: 'Google',
+    info: {
+        employees: ['Vlad', 'Olga'],
+        partners: ['Microsoft', 'Facebook', 'Xing']
+    }
+};
+
+function getInfo({name: org ='unknown',info:{partners: [a, b]}}) {
+    return console.log(`Name: ${org}, Partners: ${a},${b}`)
+}
+
+// Arrow Func.Tasks
+// Task#1
+
+const sum = (...params) => {
+    if (!params) return 0;
+    return params.reduce((prev,next) => prev + next)
+};
+
+
+
